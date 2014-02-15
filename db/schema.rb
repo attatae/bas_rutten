608c67b89bac9f2d76668b06c814753eda91fccf
Christopher de Alba <superototus@gmail.com>
2014-02-15 09:08:19 -0800

    Finish 'Migrating Statuses,' applied now to Comments also. Clear Events and Comments from db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140215074449) do
+ActiveRecord::Schema.define(version: 20140215164901) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20140215074449) do
     t.integer  "event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["event_id"], name: "index_comments_on_event_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "events", force: true do |t|
     t.string   "title"
